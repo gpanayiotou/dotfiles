20c426dcc30de4dbe5d48600c138e1f23a28648c
Georgios Panayiotou <gpanayiotou@server.fake>
2016-05-12 16:39:47 +0200

    Vim: Remove install scripts

--- Vim/install_neobundle_linux.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
--- Vim/install_neobundle_win.bat
+++
@@ -1,2 +0,0 @@
-md "%HOMEDRIVE%%HOMEPATH%/vimfiles/bundle"
-git clone https://github.com/Shougo/neobundle.vim "%HOMEDRIVE%%HOMEPATH%/vimfiles/bundle/neobundle.vim"
