a715a8e21e9bd18c9cc03081beaaae5e46c8f507
Georgios Panayiotou <gpanayiotou@server.fake>
2016-10-24 16:57:33 +0200

    Bash: Add README and update Linux install script

---
+++ Bash/README.md
@@ -0,0 +1,5 @@
+# Bash
+
+`.bash_profile`: Personal initialization file, executed for login shells. Add directories to PATH and define additional environment variables.  
+`.bashrc`: Per-shell startup file. Everything else.
+
--- Bash/install-bash-config.sh
+++ Bash/install-bash-config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ ! -f ~/.bashrc ]; then
+if [ ! -f $HOME/.bashrc ]; then
     ln -s $PWD/.bashrc ~/.bashrc
 fi
 
-if [ ! -f ~/.bash_profile ]; then
+if [ ! -f $HOME/.bash_profile ]; then
     ln -s $PWD/.bash_profile ~/.bash_profile
 fi
+
