e30ab84fbb31efb94171e60f04367c1db9431463
Georgios Panayiotou <gpanayiotou@server.fake>
2016-10-24 17:45:49 +0200

    Neovim: Tidy up Linux install script

--- install-neovim-config.sh
+++ install-neovim-config.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+NVIM_HOME=$HOME/.config/nvim
+
 # Soft link the config directory
-if [ ! -d "~/.config/nvim" ]; then
+if [ ! -e "$NVIM_HOME" ]; then
     ln -s $PWD/Neovim ~/.config/nvim
 fi
 
 # Install vim-plug
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -e "$NVIM_HOME/autoload/plug.vim" ]; then
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # Start Neovim and install plugins
 nvim -c "PlugInstall"
