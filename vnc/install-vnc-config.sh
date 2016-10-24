0c96aea59e69d5523e0906cf7b43a81b5e4a5bfb
Georgios Panayiotou <gpanayiotou@server.fake>
2016-10-24 13:44:17 +0200

    VNC: Update configuration
    Add user config file.
    Set default resolution in virtual mode to something more sane.
    Update Linux install script.

--- vnc/install-vnc-config.sh
+++ vnc/install-vnc-config.sh
@@ -8,3 +8,7 @@ if [ ! -f $HOME/.vnc/xconfig.custom ]; then
     ln -s $PWD/xstartup.custom ~/.vnc/xstartup.custom
 fi
 
+if [ ! -f $HOME/.vnc/config ]; then
+    ln -s $PWD/config ~/.vnc/config
+fi
+
