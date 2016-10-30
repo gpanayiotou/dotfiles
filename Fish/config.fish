a0cbeb012198f4d0052f0b0981d2a8212ed03f35
Georgios Panayiotou <gpanayiotou@server.fake>
2016-10-30 09:48:15 +0100

    Fish: Refactor structure of config file (cleanup), Add NVM configuration

--- Fish/config.fish
+++ Fish/config.fish
@@ -1,27 +1,31 @@
+# General settings
+set LANG en_US.UTF-8
+export TERM=xterm-256color
+
+
+# Oh-My-Fish
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
-
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG ~/.config/omf
-
-# Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-#bobthefish config
-set -g theme_nerd_fonts yes
-set -g theme_date_format "+%A %HH:%MM"
-
-export TERM=xterm-256color
 
 # Base16 Shell
 if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-ocean.sh
 end
 
+
+# Powerline
+# Fedora package version of the path
 set fish_function_path $fish_function_path "/usr/share/powerline/fish/"
 powerline-setup
 
-set LANG en_US.UTF-8
+
+# NodeJS Version Manager
+export NVM_DIR="/home/geo/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] and . "$NVM_DIR/nvm.sh"
 
