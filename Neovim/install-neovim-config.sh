b1245fbaa91099d2101bc63e6d1e0e0c1b4fa870
Georgios Panayiotou <gpanayiotou@server.fake>
2017-05-05 19:08:50 +0200

    Neovim: Add vim-plug download to install script

--- Neovim/install-neovim-config.sh
+++ Neovim/install-neovim-config.sh
@@ -9,11 +9,18 @@ if [ ! -e "$NVIM_CONFIG_HOME" ]; then
 fi
 
 # Create the cache directories
-if [ ! -e $NVIM_CACHE_HOME]; then
+if [ ! -e "$NVIM_CACHE_HOME" ]; then
     mkdir $NVIM_CACHE_HOME
     mkdir $NVIM_CACHE_HOME/backup
     mkdir $NVIM_CACHE_HOME/undo
     mkdir $NVIM_CACHE_HOME/swap
+fi
+
+# Install vim-plug
+PLUG_FILE=$HOME/.local/share/nvim/site/autoload/plug.vim
+
+if [ ! -e "$PLUG_FILE" ]; then
+    curl -fLo $PLUG_FILE --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Start Neovim and install plugins
