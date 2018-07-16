a48c8cbb2aacc7a0b611600ff4eccb451f79b316
Georgios Panayiotou <geo@geo-VM.localdomain>
2018-07-16 07:35:55 +0200

    Zsh: Add base16-shell install step to install script
    Add checks for existing directories

--- Zsh/install-zsh-config.sh
+++ Zsh/install-zsh-config.sh
@@ -6,5 +6,12 @@ if [ ! -e "$HOME/.zshrc" ]; then
 fi
 
 # Download and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Download base16-shell
+if [ ! -d "$HOME/.config/base16-shell" ]; then
+  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
 
