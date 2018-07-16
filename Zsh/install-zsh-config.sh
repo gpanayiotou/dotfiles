0612c822e42728881ccbad0ce0e6a9214721b4e4
Georgios Panayiotou <geo@geo-VM.localdomain>
2018-07-16 07:24:15 +0200

    Zsh: Add oh-my-zsh install step to install script

--- Zsh/install-zsh-config.sh
+++ Zsh/install-zsh-config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Link the config file to home dir
 if [ ! -e "$HOME/.zshrc" ]; then
     ln -s $PWD/.zshrc ~/.zshrc
 fi
+
+# Download and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
